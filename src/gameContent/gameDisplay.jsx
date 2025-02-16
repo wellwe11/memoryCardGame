5db4401937a8474fbfb20837698f106205a9e505
Robin Ryan <robin1ryan@hotmail.com>
2025-02-16 16:07:39 +0100

    changed around with display flex and grid to see what works best. Decided on flex for pokemon cards container

--- src/gameContent/gameDisplay.jsx
+++ src/gameContent/gameDisplay.jsx
@@ -64,7 +64,7 @@ function Card({ pokemonName }) {
 }
 
 function MainContent() {
-  const someArray = [1, 2, 3, 4, 5, 6, 7];
+  const someArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
     <div className="mainContentClass">
