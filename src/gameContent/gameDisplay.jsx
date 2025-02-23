2772d591c67fd9404336859dc76e181f4b54dc1b
Robin Ryan <robin1ryan@hotmail.com>
2025-02-23 15:04:52 +0100

    fixing logic to update score correctly

--- src/gameContent/gameDisplay.jsx
+++ src/gameContent/gameDisplay.jsx
@@ -4,6 +4,8 @@ import "./gameContentCSS/scoreBoard.css";
 import "./gameContentCSS/mainContent.css";
 import "./gameContentCSS/card.css";
 
+import { useState, useEffect } from "react";
+
 function HeaderContent() {
   return (
     <header className="headerContent">
@@ -24,29 +26,31 @@ function HeaderContent() {
   );
 }
 
-function ScoreBoard() {
+function ScoreBoard({ currScore, maxScore, bestScore }) {
   return (
     <div className="scoreBoardContainer">
       <div className="scoreBoardHeader">
         <div>
-          <h4>Score: 2</h4>
+          <h4>Score: {currScore}</h4>
         </div>
         <div>
-          <h4>Best Score: 8</h4>
+          <h4>Best Score: {bestScore}</h4>
         </div>
       </div>
       <div className="currentScoreTrackerContainer">
         <div className="currentScoreTrackerWrapper">
-          <h3>2 / 9</h3>
+          <h3>
+            {currScore} / {maxScore}
+          </h3>
         </div>
       </div>
     </div>
   );
 }
 
-function Card({ pokemonName }) {
+function Card({ pokemonName, id, onClick }) {
   return (
-    <div className="cardContainer">
+    <div className="cardContainer" id={id} onClick={onClick}>
       <div className="cardWrapper">
         <div className="cardHeader">
           <h5>{pokemonName}</h5>
@@ -66,12 +70,55 @@ function Card({ pokemonName }) {
 function MainContent() {
   const someArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [clickedNumbers, setClickedNumbers] = useState([]);
+  const [clickedNumber, setClickedNumber] = useState();
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore((prevBestScore) => prevBestScore + 1);
+    }
+  }, [score, bestScore]);
+
+  const cardClicked = (e) => {
+    if (e.target === e.currentTarget) {
+      setClickedNumber(Number(e.target.id));
+      if (!clickedNumbers.includes(e.target.id)) {
+        setClickedNumbers((prev) => [...prev, Number(e.target.id)]);
+        console.log(clickedNumbers);
+        setScore((prevScore) => prevScore + 1);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (clickedNumbers.includes(clickedNumber)) {
+      setScore(0);
+      setClickedNumber(null);
+      setClickedNumbers([]);
+    }
+  }, [clickedNumbers, clickedNumber, score]);
+
+  console.log(
+    "score:",
+    score,
+    "clicked number:",
+    clickedNumber,
+    "array:",
+    clickedNumbers
+  );
+
   return (
     <div className="mainContentClass">
-      <ScoreBoard />
+      <ScoreBoard
+        currScore={score}
+        maxScore={someArray.length}
+        bestScore={bestScore}
+      />
       <div className="cardBoardContainer">
         {someArray.map((el) => (
-          <Card pokemonName={el} key={el} />
+          <Card pokemonName={el} key={el} id={el} onClick={cardClicked} />
         ))}
       </div>
     </div>
